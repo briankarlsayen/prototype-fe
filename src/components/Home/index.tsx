a96514ae98e44f407b867f22cdaa31e91e952c2f
BrianSayen <blu3fire89@gmail.com>
2023-01-13 17:01:21 +0800

    fix: mobile resp home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='w-full flex flex-col justify-center'>
-      <div className='w-[50rem] text-center m-auto'>
+      <div className='w-full max-w-[50rem] text-center m-auto'>
         <h1 className='text-3xl pb-8'>Welcome to the Darkside!</h1>
         <p className='text-base'>This is a prototyping environment for me and only me. You can take a look at my prowess and be amazed at what humanity can do to make life easier. I have created a world of my own, a world where I can explore and experiment with ideas, a world where I can make mistakes and learn from them, a world where I can create something from nothing.</p>
       </div>
