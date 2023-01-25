fa7501255c61d447e771c19d699f5f8f27486b43
BrianSayen <blu3fire89@gmail.com>
2023-01-25 17:00:33 +0800

    feat: move server to vercel

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "https://SmoggyFrighteningEnvironments.blu3fire89.repl.co",
+    baseURL: "https://openai-server-eight.vercel.app",
+    // baseURL: "https://SmoggyFrighteningEnvironments.blu3fire89.repl.co",
     // baseURL: "https://openai-server.onrender.com",
 });
 
