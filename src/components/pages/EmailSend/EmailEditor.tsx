5f9474ffe95507da3b9057d55f5e65e0e0477d88
BrianSayen <blu3fire89@gmail.com>
2023-02-03 17:48:40 +0800

    update

---
+++ src/components/pages/EmailSend/EmailEditor.tsx
@@ -0,0 +1,28 @@
+import EmailEditor from "react-email-editor";
+// import template from "./design001.json";
+
+const Editor = ({ emailEditorRef, setSaved }: any) => {
+  const onLoad = () => {
+    const template = localStorage.getItem("design");
+    if (template)
+      emailEditorRef.current.editor.loadDesign(JSON.parse(template));
+    emailEditorRef.current.editor.addEventListener(
+      "design:updated",
+      function () {
+        setSaved(false);
+      }
+    );
+  };
+
+  const onReady = () => {
+    console.log("onReady");
+  };
+
+  return (
+    <div>
+      <EmailEditor ref={emailEditorRef} onLoad={onLoad} onReady={onReady} />
+    </div>
+  );
+};
+
+export default Editor;
--- src/components/pages/EmailSend/index.tsx
+++ src/components/pages/EmailSend/index.tsx
@@ -1,16 +1,30 @@
-import React, { useState, KeyboardEventHandler } from "react";
+import { useState, KeyboardEventHandler, useRef } from "react";
 import CreatableSelect from "react-select/creatable";
+import Editor from "./EmailEditor";
 
 interface Option {
   readonly label: string;
   readonly value: string;
 }
 
+interface ExportProps {
+  design: string;
+  html: string;
+}
+
+// TODO make container with email message
+// TODO save multiple templates, save in json file
+// TODO validate if email is valid
+// TODO send email to multiple users
 const EmailSend = () => {
   const [spinAction, setSpinAction] = useState(false);
   const [value, setValue] = useState<readonly Option[]>([]);
   const [inputValue, setInputValue] = useState("");
-  // const [emailArr, setEmailArr] = useState<Array<string>>([]);
+  const [isWarning, setWarning] = useState(false);
+  const [isSaved, setSaved] = useState(false);
+
+  const emailEditorRef = useRef<any>(null);
+
   const generateHandler = async (e: any) => {
     e.preventDefault();
     setSpinAction(true);
@@ -19,12 +33,6 @@ const EmailSend = () => {
       setSpinAction(false);
     }, 5000);
   };
-
-  // const selectedArr = [
-  //   "mail@mail.com",
-  //   "jinsei@mail.com",
-
-  // ]
 
   const createOption = (label: string) => ({
     label,
@@ -37,6 +45,7 @@ const EmailSend = () => {
   };
 
   const handleAddEmail: KeyboardEventHandler = async (event) => {
+    setWarning(false);
     if (!inputValue) return;
     switch (event.key) {
       case "Enter":
@@ -45,6 +54,7 @@ const EmailSend = () => {
           setValue((prev) => [...prev, createOption(inputValue)]);
           setInputValue("");
         } else {
+          setWarning(true);
           console.log("Email not valid");
         }
         event.preventDefault();
@@ -55,20 +65,40 @@ const EmailSend = () => {
     DropdownIndicator: null,
   };
 
+  const exportHtml = () => {
+    if (emailEditorRef.current) {
+      emailEditorRef.current.editor.exportHtml((data: ExportProps) => {
+        const { design, html } = data;
+        console.log("exportHtml", html);
+      });
+    }
+  };
+
+  const saveJsonFile = (jsonData: any, filename: string) => {
+    const fileData = JSON.stringify(jsonData);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.style.cssText = "position:absolute";
+
+    link.download = `${filename}.json`;
+    link.href = url;
+    link.click();
+  };
+
+  const handleSave = () => {
+    const design = emailEditorRef.current.editor.saveDesign(
+      async (design: any) => {
+        // saveJsonFile(design, "dummy");
+        localStorage.setItem("design", JSON.stringify(design));
+        setSaved(true);
+      }
+    );
+    // console.log("design", design);
+  };
+
   return (
     <div className="flex gap-4 h-full ">
-      <div className="toast toast-top toast-end">
-        <div className="alert alert-info">
-          <div>
-            <span>New mail arrived.</span>
-          </div>
-        </div>
-        <div className="alert alert-success">
-          <div>
-            <span>Message sent successfully.</span>
-          </div>
-        </div>
-      </div>
       <section className="basis-1/3 bg-slate-700 p-4 border border-white rounded-md">
         <h1 className="pb-8">Generate:</h1>
         <form onSubmit={generateHandler}>
@@ -76,12 +106,12 @@ const EmailSend = () => {
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 ">
               Email Address
             </label>
-
-            {/* <input
-              type="text"
-              placeholder="email address"
-              className="input input-bordered w-full"
-            /> */}
+            {isWarning && (
+              <div
+                className="tooltip tooltip-open tooltip-warning"
+                data-tip="input correct email format"
+              ></div>
+            )}
             <CreatableSelect
               components={components}
               isMulti
@@ -92,6 +122,7 @@ const EmailSend = () => {
               inputValue={inputValue}
               onInputChange={(newValue) => setInputValue(newValue)}
               menuIsOpen={false}
+              placeholder="email address"
             />
           </div>
           <div className="form-control pb-4">
@@ -118,15 +149,14 @@ const EmailSend = () => {
               <option>casual</option>
             </select>
           </div>
-          {/* <button
+          <button
             type="submit"
             className={`mt-8 float-right btn btn-outline btn-info ${
               spinAction && "loading"
             }`}
-            // className="mt-8 float-right btn btn-outline btn-info loading"
           >
             Generate
-          </button> */}
+          </button>
         </form>
       </section>
       <section className="basis-2/3 bg-slate-700 p-4 border border-white rounded-md">
@@ -135,7 +165,28 @@ const EmailSend = () => {
           className="textarea textarea-bordered w-full"
           disabled
         ></textarea> */}
-        <div className="bg-[#2A303C] border border-gray-600 h-32 rounded-md"></div>
+        {/* <div className="bg-[#2A303C] border border-gray-600 h-32 rounded-md"></div> */}
+        <div>
+          <button className="btn" onClick={exportHtml}>
+            Export HTML
+          </button>
+        </div>
+        <Editor
+          emailEditorRef={emailEditorRef}
+          exportHtml={exportHtml}
+          setSaved={setSaved}
+        />
+        <div className="flex flex-row-reverse">
+          {isSaved ? (
+            <button onClick={handleSave} className="btn mt-4" disabled>
+              Saved
+            </button>
+          ) : (
+            <button onClick={handleSave} className="btn mt-4">
+              Save
+            </button>
+          )}
+        </div>
       </section>
     </div>
   );
